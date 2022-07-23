416ef798c5204c8865def70b2fd67fc6d2d545cb
auropoint <auropoint@gmail.com>
2022-07-23 22:29:35 +0300

    after 21 module

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -8,6 +8,10 @@ public class SquareTests {
   @Test
   public void TestArea() {
     Square s = new Square(5);
+
+//    assert s.area() == 24;
+
+    //в этом варианте при ошибке будет показано и вычисленное, и ожидаемое значение
     Assert.assertEquals(s.area(), 25);
 
   }
