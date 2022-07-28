a7c7531101ca9d7a18ec92cd4542c0ca24450eba
auropoint <auropoint@gmail.com>
2022-07-28 10:33:29 +0300

    3-37

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -8,6 +8,11 @@ public class GroupModificationTest extends TestBase {
   @Test
   public void testGroupModififcation() {
     app.getNavigationHelper().gotoGroupPage();
+
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
+
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
