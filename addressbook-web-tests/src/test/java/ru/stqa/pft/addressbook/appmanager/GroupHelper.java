575b53cca536fdfd43340a8e980814c428c0a928
auropoint <auropoint@gmail.com>
2022-07-30 16:19:22 +0300

    4-42

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,7 +2,11 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase {
 
@@ -61,5 +65,13 @@ public class GroupHelper extends HelperBase {
   }
 
 
-
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement k : elements) {
+      String name = k.getText();
+      groups.add(new GroupData(name, null, null));
+    }
+    return groups;
+  }
 }
