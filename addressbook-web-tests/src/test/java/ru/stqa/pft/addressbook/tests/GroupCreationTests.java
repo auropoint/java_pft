e5911f626a5a91b6d51a9f8209ef139e2b4d197d
auropoint <auropoint@gmail.com>
2022-07-31 21:52:27 +0300

    4-46 лямбда выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,17 +13,44 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test8", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
+
+    //Нахождение максимального айди группы
+
+
+    // 1) путём перебора циклом
+//    int max = 0;
+//    for (GroupData g : after) {
+//      if (g.getId() > max) {
+//        max = g.getId();
+//      }
+//    }
+
+
+    //путём возможностей Java 8, используя старые средства (анонимный класс)
+//    Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+//      @Override
+//      public int compare(GroupData o1, GroupData o2) {
+//        return Integer.compare(o1.getId(), o2.getId());
+//      }
+//    };
+//
+//    int max = after.stream().max(byId).get().getId();
+
+
+    //путём возможностей Java 8, используя лямбда-выражения (анонимные функции)
+
+//    Comparator<? super GroupData> byId =
+//            (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+
+    int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+
+
+
 
     group.setId(max);
     before.add(group);
