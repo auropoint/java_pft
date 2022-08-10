d4efab3dbee0bd078d5bc9c8c282a8634b17caff
auropoint <auropoint@gmail.com>
2022-08-10 22:40:23 +0300

    6-62 - параметризация тестовых данных. DataProvider. Вывод тестовых данных в отчёте выполнения теста.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,29 +1,42 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<>();
+    list.add(new Object[]{new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[]{new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[]{new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+    return list.iterator();
+  }
+
   @BeforeMethod
   private void ensurePreconditions() {
     app.goTo().groupPage();
   }
 
-  @Test
-  public void testGroupCreation() {
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test8");
-    app.group().create(group);
-    assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
-    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-    assertThat(after, equalTo(before.withAdded(group)));
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
+      Groups before = app.group().all();
+      app.group().create(group);
+      assertThat(app.group().count(), equalTo(before.size() + 1));
+      Groups after = app.group().all();
+      group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
+      assertThat(after, equalTo(before.withAdded(group)));
   }
 
   @Test
