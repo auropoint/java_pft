3edc42e06580bafd4b732db462311f0318225ec7
auropoint <auropoint@gmail.com>
2022-08-14 18:15:25 +0300

    7-71 - ORM – object relational mapping

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -13,8 +13,8 @@ public class DbConnectionTest {
     Connection conn = null;
     try {
       conn =
-              DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?" +
-                      "user=root&password=");
+              DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook" +
+                      "?user=root&password=");
 
       Statement st = conn.createStatement();
       ResultSet rs = st.executeQuery("select group_id,group_name, group_header,group_footer from group_list");
