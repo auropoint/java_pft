ffb7b981b6ce2badc2095daf13e219e123eb7fcd
auropoint <auropoint@gmail.com>
2022-08-18 17:18:04 +0300

    8.77 - Шаблон проекта. Подготовка для тестирования MantisBT

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,44 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class ApplicationManager {
+
+  private final Properties properties;
+  WebDriver wd;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    if (browser.equals("firefox")) {
+      wd = new FirefoxDriver();
+    } else if (browser.equals("chrome")) {
+      wd = new ChromeDriver();
+    } else if (browser.equals("ie")) {
+      wd = new InternetExplorerDriver();
+    }
+
+    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+    wd.get(properties.getProperty("web.baseUrl"));
+  }
+
+  public void stop() {
+    wd.quit();
+  }
+}
