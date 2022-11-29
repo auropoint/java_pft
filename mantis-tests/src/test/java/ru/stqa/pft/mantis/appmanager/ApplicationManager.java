e789d312205d5f376420cf517deb1521ec8c4bd8
auropoint <auropoint@gmail.com>
2022-11-29 23:30:48 +0300

    8.79 - Ленивая инициализация помощников

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -14,8 +14,9 @@ import java.util.Properties;
 public class ApplicationManager {
 
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -25,21 +26,12 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    if (browser.equals("firefox")) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals("chrome")) {
-      wd = new ChromeDriver();
-    } else if (browser.equals("ie")) {
-      wd = new InternetExplorerDriver();
-    }
-
-    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-    wd.get(properties.getProperty("web.baseUrl"));
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
 
   public HttpSession newSession() {
@@ -48,5 +40,28 @@ public class ApplicationManager {
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null) {
+      if (browser.equals("firefox")) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals("chrome")) {
+        wd = new ChromeDriver();
+      } else if (browser.equals("ie")) {
+        wd = new InternetExplorerDriver();
+      }
+
+      wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
